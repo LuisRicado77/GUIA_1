3906138de85168aeca566a957ab084bf25f6f8f3
LuisRicardo77 <luissayas2018@gmail.com>
2024-04-02 19:02:48 -0500

    primer commit

---
+++ practicaSpring/src/main/java/com/colombo/practica/practicaSpring/PracticaSpringApplication.java
@@ -0,0 +1,20 @@
+package com.colombo.practica.practicaSpring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PracticaSpringApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(PracticaSpringApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
